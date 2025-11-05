74f2815633c02dcd5727f6be0c25d07e3a8ca9dc
danielsousaazevdo <daniel.sousaazevdo@gmail.com>
2025-11-05 14:49:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function gerar() {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*"
-    const tam = document.getElementoById('tam').value
-    let senha = ''
-    for (let i = 0; i < tam; i++) {
-        senha += chars.charAt(Math.floor(Math.random() * chars.length))
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
+    const tam = Number(document.getElementById("tam").value);
+
+    if (!tam || tam <= 0) {
+        alert("Digite um tamanho válido para a senha!");
+        return;
     }
+
+    let senha = '';
+    for (let i = 0; i < tam; i++) {
+        senha += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
     document.getElementById('res').innerText = `Senha gerada: ${senha}`;
 }
