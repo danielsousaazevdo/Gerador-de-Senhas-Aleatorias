98e0d956faa186cd8d126de4178f7a8d699247cb
danielsousaazevdo <daniel.sousaazevdo@gmail.com>
2025-11-05 14:45:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function gerar() {
     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*"
-    const tam = document.getElementByID("tam".value)
+    const tam = (document.getElementById("tam").value)
     let senha = ''
     for (let i = 0; i < tam; i++) {
         senha += chars.charAt(Math.floor(Math.random() * chars.length))
